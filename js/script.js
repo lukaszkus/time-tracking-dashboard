7ef4f20b41b24efe3c5630df8f37ec9b3ccebf55
lukaszkus <kusiu@vp.pl>
2021-11-24 00:07:23 +0100

    script.js update

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@ const svg = `<svg class="stats__icon" width="21" height="5" xmlns="http://www.w3
             </svg>`;
 
 
+let dailyData = [];
+
 async function getData() {
     let url = 'https://raw.githubusercontent.com/lukaszkus/time-tracking-dashboard/main/data.json';
     try {
@@ -19,35 +21,7 @@ async function getData() {
     }
 }
 
-async function renderDaily() {
-  let data = await getData();
-
-    data.forEach(item => {
-      const { title, timeframes: { daily: {current, previous} } } = item;
-      const newArticle = document.createElement("article");
-
-      const hr = current !== 1 || previous !== 1 ? "hrs" : "hr";
-
-      const component = `
-        <div class="stats">
-            <div class="stats__row">
-              <h2 class="stats__title">${title}</h2>
-              ${svg}
-            </div>
-            <div class="stats__row">
-              <p class="stats__hours">${current}${hr}</p>
-              <span class="stats__date">Yesterday - ${previous}${hr}</span>
-            </div>
-        </div>
-      `;
-
-      const dashBoard = document.querySelector('.dashboard');
-      dashBoard.appendChild(newArticle).innerHTML = component;
-      newArticle.className = `card card--${title.toLowerCase()} card__stats`;
-    });
-};
-
-async function renderWeekly() {
+async function renderData() {
   let data = await getData();
 
     data.forEach(item => {
@@ -64,7 +38,7 @@ async function renderWeekly() {
             </div>
             <div class="stats__row">
               <p class="stats__hours">${current}${hr}</p>
-              <span class="stats__date">Last week - ${previous}${hr}</span>
+              <span class="stats__date">Last Week - ${previous}${hr}</span>
             </div>
         </div>
       `;
@@ -74,45 +48,93 @@ async function renderWeekly() {
       newArticle.className = `card card--${title.toLowerCase()} card__stats`;
     });
 };
-renderWeekly();
+renderData();
+
+
+
+async function renderDaily() {
+  let data = await getData();
+
+  let dailyCurrent = [];
+  let dailyPrevious = [];
+
+  const statsHours = document.querySelectorAll(".stats__hours");
+  const statsDate = document.querySelectorAll(".stats__date");
+
+  data.forEach(item => {
+    let { timeframes: { daily: {current, previous} } } = item;
+    dailyCurrent.push(current);
+    dailyPrevious.push(previous);
+  });
+
+  for (i = 0; i < statsHours.length; i++) {
+    let hr = i === 1 ? "hr" : "hrs";
+    statsHours[i].innerHTML = `${dailyCurrent[i]}${hr}`;
+  }
+
+  for (i = 0; i < statsDate.length; i++) {
+    let hr = i === 1 ? "hr" : "hrs";
+    statsDate[i].innerHTML = `Yesterday - ${dailyPrevious[i]}${hr}`;
+  }
+}
+
+async function renderWeekly() {
+  let data = await getData();
+
+  let weeklyCurrent = [];
+  let weeklyPrevious = [];
+
+  const statsHours = document.querySelectorAll(".stats__hours");
+  const statsDate = document.querySelectorAll(".stats__date");
+
+  data.forEach(item => {
+    let { timeframes: { weekly: {current, previous} } } = item;
+    weeklyCurrent.push(current);
+    weeklyPrevious.push(previous);
+  });
+
+  for (i = 0; i < statsHours.length; i++) {
+    let hr = i === 1 ? "hr" : "hrs";
+    statsHours[i].innerHTML = `${weeklyCurrent[i]}${hr}`;
+  }
+
+  for (i = 0; i < statsDate.length; i++) {
+    let hr = i === 1 ? "hr" : "hrs";
+    statsDate[i].innerHTML = `Last Week - ${weeklyPrevious[i]}${hr}`;
+  }
+}
 
 async function renderMonthly() {
   let data = await getData();
 
-    data.forEach(item => {
-      const { title, timeframes: { monthly: {current, previous} } } = item;
-      const newArticle = document.createElement("article");
+  let monthlyCurrent = [];
+  let monthlyPrevious = [];
 
-      const hr = current !== 1 || previous !== 1 ? "hrs" : "hr";
+  const statsHours = document.querySelectorAll(".stats__hours");
+  const statsDate = document.querySelectorAll(".stats__date");
 
-      const component = `
-        <div class="stats">
-            <div class="stats__row">
-              <h2 class="stats__title">${title}</h2>
-              ${svg}
-            </div>
-            <div class="stats__row">
-              <p class="stats__hours">${current}${hr}</p>
-              <span class="stats__date">Last Month - ${previous}${hr}</span>
-            </div>
-        </div>
-      `;
+  data.forEach(item => {
+    let { timeframes: { monthly: {current, previous} } } = item;
+    monthlyCurrent.push(current);
+    monthlyPrevious.push(previous);
+  });
 
-      const dashBoard = document.querySelector('.dashboard');
-      dashBoard.appendChild(newArticle).innerHTML = component;
-      newArticle.className = `card card--${title.toLowerCase()} card__stats`;
-    });
-};
+  for (i = 0; i < statsHours.length; i++) {
+    let hr = i === 1 ? "hr" : "hrs";
+    statsHours[i].innerHTML = `${monthlyCurrent[i]}${hr}`;
+  }
 
+  for (i = 0; i < statsDate.length; i++) {
+    let hr = i === 1 ? "hr" : "hrs";
+    statsDate[i].innerHTML = `Last Month - ${monthlyPrevious[i]}${hr}`;
+  }
+}
 
-daily.addEventListener('click', function(e) {
+daily.addEventListener('click', function (e) {
   e.preventDefault();
   daily.classList.add('active');
   weekly.classList.remove('active');
   monthly.classList.remove('active');
-
-  const removeElements = (el) => el.forEach(el => el.remove());
-  removeElements(document.querySelectorAll('.card__stats'));
   
   renderDaily();
 });
@@ -123,9 +145,6 @@ weekly.addEventListener('click', function (e) {
   weekly.classList.add('active');
   daily.classList.remove('active');
   monthly.classList.remove('active');
-
-  const removeElements = (el) => el.forEach(el => el.remove());
-  removeElements(document.querySelectorAll('.card__stats'));
   
   renderWeekly();
 });
@@ -135,9 +154,6 @@ monthly.addEventListener('click', function (e) {
   monthly.classList.add('active');
   daily.classList.remove('active');
   weekly.classList.remove('active');
-
-  const removeElements = (el) => el.forEach(el => el.remove());
-  removeElements(document.querySelectorAll('.card__stats'));
   
   renderMonthly();
 });
